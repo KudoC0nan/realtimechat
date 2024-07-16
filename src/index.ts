8b5be920e06eea8dc3d61158cce0b01c243ddf3b
KudoC0nan <patilshreyasvjti@gmail.com>
2024-07-16 22:42:28 +0530

    fixed index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-#!/usr/bin/env node
-import {connection, server as WebSocketServer} from "websocket"
-import http from 'http'
-import { OutgoingMessage, SupportedMessage as OutgoingSupportedMessages } from "./messages/outgoingMessages"
-import { IncomingMessage, SupportedMessage } from "./messages/incommingMessages";
+import { OutgoingMessage, SupportedMessage as OutgoingSupportedMessages } from "./messages/outgoingMessages";
+import {server as WebSocketServer, connection} from "websocket"
+import http from 'http';
 import { UserManager } from "./UserManager";
+import { IncomingMessage, SupportedMessage } from "./messages/incomingMessages";
+
 import { InMemoryStore } from "./store/InMemoryStore";
 
 const server = http.createServer(function(request: any, response: any) {
@@ -12,6 +12,8 @@ const server = http.createServer(function(request: any, response: any) {
     response.end();
 });
 
+server
+
 const userManager = new UserManager();
 const store = new InMemoryStore();
 
@@ -19,9 +21,9 @@ server.listen(8080, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
 
-const wsServer = new WebSocketServer({
+ const wsServer = new WebSocketServer({
     httpServer: server,
-    autoAcceptConnections: true
+    autoAcceptConnections: false
 });
 
 function originIsAllowed(origin: string) {
@@ -29,6 +31,8 @@ function originIsAllowed(origin: string) {
 }
 
 wsServer.on('request', function(request) {
+    console.log("inside connect");
+
     if (!originIsAllowed(request.origin)) {
       // Make sure we only accept requests from an allowed origin
       request.reject();
@@ -39,37 +43,38 @@ wsServer.on('request', function(request) {
     var connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
-        // Todo add rate limiting logic
+
+        // Todo add rate limitting logic here 
         if (message.type === 'utf8') {
             try {
                 messageHandler(connection, JSON.parse(message.utf8Data));
-            } catch (e) {
+            } catch(e) {
 
             }
         }
     });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
 });
 
 function messageHandler(ws: connection, message: IncomingMessage) {
-    if (message.type === SupportedMessage.JoinRoom) {
-        const payload = message.payload
+    if (message.type == SupportedMessage.JoinRoom) {
+        const payload = message.payload;
         userManager.addUser(payload.name, payload.userId, payload.roomId, ws);
     }
+
     if (message.type === SupportedMessage.SendMessage) {
-        const payload = message.payload
-        const user = userManager.getUser(payload.roomId, payload.userId)
+        const payload = message.payload;
+        const user = userManager.getUser(payload.roomId, payload.userId);
+
         if (!user) {
-            console.error("User not found in db");
-            return
+            console.error("User not found in the db");
+            return;
         }
-        let chat = store.addChat(payload.userId, user.name, payload.roomId, payload.message)
+        let chat = store.addChat(payload.userId, user.name, payload.roomId, payload.message);
         if (!chat) {
             return;
         }
-        const outgoingPayload: OutgoingMessage = {
+
+        const outgoingPayload: OutgoingMessage= {
             type: OutgoingSupportedMessages.AddChat,
             payload: {
                 chatId: chat.id,
@@ -79,15 +84,19 @@ function messageHandler(ws: connection, message: IncomingMessage) {
                 upvotes: 0
             }
         }
-        userManager.broadcast(payload.roomId, payload.userId, outgoingPayload)
+        userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
     }
+
     if (message.type === SupportedMessage.UpvoteMessage) {
-        const payload = message.payload
-        const chat = store.upvote(payload.userId, payload.roomId, payload.chatId)
+        const payload = message.payload;
+        const chat = store.upvote(payload.userId, payload.roomId, payload.chatId);
+        console.log("inside upvote")
         if (!chat) {
-            return
+            return;
         }
-        const outgoingPayload: OutgoingMessage = {
+        console.log("inside upvote 2")
+
+        const outgoingPayload: OutgoingMessage= {
             type: OutgoingSupportedMessages.UpdateChat,
             payload: {
                 chatId: payload.chatId,
@@ -95,6 +104,8 @@ function messageHandler(ws: connection, message: IncomingMessage) {
                 upvotes: chat.upvotes.length
             }
         }
-        userManager.broadcast(payload.roomId, payload.userId, outgoingPayload)
+
+        // console.log("inside upvote 3")
+        userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
     }
 }
